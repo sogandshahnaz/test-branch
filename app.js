bba0d9f5c734a68370d5169ab418f721e8741d1d
Celine <sogandshahnaz@gmail.com>
2025-04-19 14:08:09 +0330

    add some code to js

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ const user = {
 let userStreet = user.address.city?.street;
 
 console.log(userStreet);
+
+
+const obj = {
+    name: 'iphone',
+    brand: 'apple'
+};
+
+const entries = Object.entries(obj);
+
+console.log(entries);
+
+let mySet = new Set();
+mySet.add(1);
+mySet.add(2);
+mySet.add(1);
+
+console.log(mySet);
