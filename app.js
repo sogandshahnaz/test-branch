e5a96c4acf5f3abcac11b10c04acf0f4adc2e84a
Celine <sogandshahnaz@gmail.com>
2025-04-19 12:43:12 +0330

    practiced some js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log('hello world');
-console.log('how are you');
+let port = null;
+let developingPort = port ?? 3000;
+
+console.log(developingPort);
+
+const user = {
+    name: 'celine',
+    age: 19,
+    address: {
+        city: 'LA'
+    }
+};
+
+let userStreet = user.address.city?.street;
+
+console.log(userStreet);
