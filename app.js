72fb8a6bdd390d1c2cbba3e4a151ba5e740c82eb
Celine <sogandshahnaz@gmail.com>
2025-04-20 17:05:40 +0330

    practice proxy property in js

--- app.js
+++ app.js
@@ -1,33 +1,100 @@
-let port = null;
-let developingPort = port ?? 3000;
+// let port = null;
+// let developingPort = port ?? 3000;
 
-console.log(developingPort);
+// console.log(developingPort);
 
-const user = {
-    name: 'celine',
-    age: 19,
-    address: {
-        city: 'LA'
+// const person = {
+//     name: 'celine',
+//     age: 19,
+//     address: {
+//         city: 'LA'
+//     }
+// };
+
+// let userStreet = person.address.city?.street;
+
+// console.log(userStreet);
+
+
+// const obj = {
+//     name: 'iphone',
+//     brand: 'apple'
+// };
+
+// const entries = Object.entries(obj);
+
+// console.log(entries);
+
+// let mySet = new Set();
+// mySet.add(1);
+// mySet.add(2);
+// mySet.add(1);
+
+// console.log(mySet);
+
+
+// const user = {
+//     name: 'celine',
+//     age: 25,
+//     country: 'USA',
+//     city: 'Los angeles'
+// };
+
+// const proxyUser = new Proxy(user, {
+//     get(target, property){
+//         console.log(`${property}`);
+//         return target[property];
+//     },
+//     set(target, property, value){
+//         console.log(`property: ${property} value: ${value}`);
+//         target[property] = value;
+//         return true
+//     }
+// });
+// console.log(proxyUser.name);
+// proxyUser.job = 'singer and programmer';
+
+const phone = {
+    name: 'iPhone',
+    brand: 'Apple',
+    coutry: 'USA'
+};
+
+const proxyPhone = new Proxy(phone, {
+    get(target, property){
+        console.log(`getting ${property}`);
+        return target[property];
+    },
+    set(target, pro, value){
+        console.log(`setting ${pro} with the value of ${value}`);
+        target[pro] = value;
+        return true;
     }
+});
+
+console.log(proxyPhone.name);
+proxyPhone.cost = 850000;
+
+
+const userIdentity = {
+    name: 'billie eilish',
+    age: 23,
+    job: 'singer',
+    ig: '@billieeilish',
+    password: '1478373'
 };
 
-let userStreet = user.address.city?.street;
+const proxy = new Proxy(userIdentity, {
+    deleteProperty(target, prop){
+        if(prop === 'password' || prop === 'name'){
+            console.log(`${prop} cannot be deleted `);
+            return false
+        } else {
+            console.log(`${prop} deleted`);
+            return delete target[prop]
+        }
+    }
+});
 
-console.log(userStreet);
-
-
-const obj = {
-    name: 'iphone',
-    brand: 'apple'
-};
-
-const entries = Object.entries(obj);
-
-console.log(entries);
-
-let mySet = new Set();
-mySet.add(1);
-mySet.add(2);
-mySet.add(1);
-
-console.log(mySet);
+delete proxy.age;
+delete proxy.name;
